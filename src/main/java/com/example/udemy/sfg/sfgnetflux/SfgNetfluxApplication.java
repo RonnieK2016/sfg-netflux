6ed5859a3b4e1c674624c50e49b5299a054552a1
SergeA <ronnieo@gmail.com>
2019-04-18 22:19:50 -0400

    Initial Commit

---
+++ src/main/java/com/example/udemy/sfg/sfgnetflux/SfgNetfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemy.sfg.sfgnetflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgNetfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgNetfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/udemy/sfg/sfgnetflux/SfgNetfluxApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.udemy.sfg.sfgnetflux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgNetfluxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
